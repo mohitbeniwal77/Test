f715844acd9342dc844de026d1e6bbc8db432dbc
mohitbeniwal77 <mohitbeniwal2025@gmail.com>
2024-12-09 13:00:07 +0530

    kjhgfd

--- Hello.java
+++ Hello.java
@@ -16,7 +16,7 @@ class Welcome
 
 class Wel
 {
-	public static void main(String[] args) 
+	public static void main(String[] args)
 	{
 		System.out.println("Wel");
 	}
---
+++ Mobile.java
@@ -0,0 +1,15 @@
+class Mobile
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Mobile");
+	}
+}
+
+class Android
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Android");
+	}
+}
